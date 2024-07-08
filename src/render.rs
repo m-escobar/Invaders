388806bb130ced7ec3a0ff19ed2d70e591f4964e
Marcelo Escobar <mers.escobar@gmail.com>
2024-07-08 20:13:19 -0300

    remove non original audio files

--- src/render.rs
+++ src/render.rs
@@ -7,7 +7,7 @@ use crate::frame::Frame;
 
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, curr_frame: &Frame, force: bool) {
     if force {
-        stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::DarkGrey)).unwrap();
         stdout.queue(Clear(ClearType::All)).unwrap();
         stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
         stdout.queue(SetForegroundColor(Color::White)).unwrap();
